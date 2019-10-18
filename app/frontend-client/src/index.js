d58cb7c3b5ab5ac9f647b2abda3def1115c506cb
Jorge Chavez <jorge.chavez@points.com>
2019-10-18 12:35:22 -0600

    Add events routes

--- app/frontend-client/src/index.js
+++ app/frontend-client/src/index.js
@@ -4,18 +4,16 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import '../styles/app.scss';
 
 // Pages
-import Home from './js/pages/Home';
-import Events from './js/pages/Events';
-import SingleEvent from './js/pages/SingleEvent';
-import SignInEvent from './js/pages/SignInEvent';
+import Home from './js/components/pages/home/Home';
+import EventRoutes from './js/routes/EventRoutes';
+import NotFound from './js/components/NotFound.js';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/events" exact component={Events} />
-      <Route path="/event/:eventId" exact component={SingleEvent} />
-      <Route path="/event/:eventId/signin" exact component={SignInEvent} />
+      <Route path="/events" component={EventRoutes} />
+      <Route render={() => <NotFound />} />
     </Switch>
   </Router>
 );
--- app/frontend-client/src/js/components/pages/events/SignInEvent.js
+++ app/frontend-client/src/js/components/pages/events/SignInEvent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 class SignInEvent extends Component {
+
   render() {
+    const { params: { eventId } } = this.props.match;
     return (
-      <h1>Inscribete al evento</h1>
+      <h1>Inscribete al evento {eventId}</h1>
     );
   }
 }
